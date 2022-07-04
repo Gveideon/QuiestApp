ed04ac24cf0f3ba6080b2c9374c83ca640dd6608
Kreol1942 <tema123.ss98@gmail.com>
2022-07-04 23:14:41 +0300

    probably worked API

--- QuiestConsole/Program.cs
+++ QuiestConsole/Program.cs
@@ -6,16 +6,7 @@ namespace QuestConsole
     {
         public static void Main(string[] args)
         {
-            int a = 0;
-            int b = 2;
-            (a, b) = (b, a);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine("byeeeee");
-            Console.WriteLine("byeeeee");
-            Console.WriteLine("byeee");
-            (b, a) = (a, b);
-            Console.ReadKey();
+
         }
     }
    
