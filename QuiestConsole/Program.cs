29a040c8ab754a090947f3922e294037fedcd097
Artem <Beaa.21@uni-dubna.ru>
2022-07-04 16:22:56 +0300

    New commit for cheked

--- QuiestConsole/Program.cs
+++ QuiestConsole/Program.cs
@@ -7,6 +7,7 @@ namespace QuestConsole
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Ebeny");
         }
     }
 }
