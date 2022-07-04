fff161f3484aeefc8e45e96e735141dbc107af68
Kreol1942 <tema123.ss98@gmail.com>
2022-07-04 20:11:48 +0300

    Create empty API  interface

---
+++ QuestEngine/GameController.cs
@@ -0,0 +1,16 @@
+﻿namespace QuestEngine
+{
+    public class GameController
+    {
+        public string CurrentPhrase { get; private set; }
+        public string CurrentNumberbacground { get; private set; }
+        public GameController() { }
+        public void Init() { }
+        public void Reset() { }
+        public void MakeAggressiveAction() { }
+        public void MakeFriendlyAction() { }
+        public void MakeIgnoreAction() { }
+
+    }
+
+}
