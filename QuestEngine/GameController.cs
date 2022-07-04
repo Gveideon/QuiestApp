604b91475e9bd16660c75ae5989e92f954d50f90
Kreol1942 <tema123.ss98@gmail.com>
2022-07-04 21:11:38 +0300

    update interface

--- QuestEngine/GameController.cs
+++ QuestEngine/GameController.cs
@@ -1,9 +1,12 @@
-﻿namespace QuestEngine
+﻿using System.Text.Json;
+using System.IO;
+namespace QuestEngine
 {
     public class GameController
     {
         public string CurrentPhrase { get; private set; }
         public string CurrentNumberbacground { get; private set; }
+        public bool IsEnd { get; private set; }
         public GameController() { }
         public void Init() { }
         public void Reset() { }
