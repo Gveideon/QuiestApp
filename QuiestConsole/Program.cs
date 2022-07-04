e1be1e3926ce7cd3fd87061405b5353521207453
Victor <67371337+Gveideon@users.noreply.github.com>
2022-07-04 17:06:00 +0300

    Revert "ioioio"

--- QuiestConsole/Program.cs
+++ QuiestConsole/Program.cs
@@ -6,10 +6,6 @@ namespace QuestConsole
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("bye1");
-            Console.WriteLine("bye2");
-            Console.WriteLine("bye3");
-
             int a = 0;
             int b = 2;
             (a, b) = (b, a);
