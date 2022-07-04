6663f75ba6fdd28c1ae3d4904a0f30fde2533cfb
Kreol1942 <tema123.ss98@gmail.com>
2022-07-04 16:57:15 +0300

    ioioio

--- QuiestConsole/Program.cs
+++ QuiestConsole/Program.cs
@@ -6,9 +6,9 @@ namespace QuestConsole
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("bye");
-            Console.WriteLine("bye");
-            Console.WriteLine("bye");
+            Console.WriteLine("bye1");
+            Console.WriteLine("bye2");
+            Console.WriteLine("bye3");
 
             Console.ReadKey();
         }
